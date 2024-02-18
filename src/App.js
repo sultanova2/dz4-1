13d275a1e0ebc3b4aab51b28f6ea06ddba14cf66
sultanova303 <151714059+sultanova303@users.noreply.github.com>
2024-02-19 00:24:44 +0600

    START-33

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function Profile() {
+  return <img src="https://hips.hearstapps.com/hmg-prod/images/apple-ceo-steve-jobs-speaks-during-an-apple-special-event-news-photo-1683661736.jpg?crop=0.800xw:0.563xh;0.0657xw,0.0147xh&resize=1200:*" alt="Kathrine Jonson"></img>
+  }
+export default function Gallery() {
+  return (
+    <section>
+    <h1>The best businessmen in the world $</h1> 
+    <p>He was the best businessman in the world.his name is Steve Jobs. HE IS Co-Founder  of Apple Corporation.</p>
+    <p>  He is American  entrepreneur, designer and investor. </p>
+     <p> And one of the founders and CEO of Pixar film studio.</p>
+    
+    <Profile/>
+    <Profile/>git 
+    <Profile/>
+    <Profile/>
+    </section>
+  );
+}
